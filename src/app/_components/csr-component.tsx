6f26bff00ecf4477cbb3a8b74b02f50d46b3808a
Minh Lai <nhatminhngo11@gmail.com>
2025-01-19 16:56:11 +0700

    feat: test csr component

---
+++ src/app/_components/csr-component.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function CSRComponent() {
+  return <div>Client side Text</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
 import Container from '@/components/Container';
+
+const CSRComponent = dynamic(
+  () => import('./_components/csr-component').then(mod => mod.default),
+  {
+    ssr: false,
+  },
+);
 
 export default function Home() {
   return (
     <main>
-      <Container>Testing</Container>
+      <Container>
+        <CSRComponent />
+      </Container>
     </main>
   );
 }
